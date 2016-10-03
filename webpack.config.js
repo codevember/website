d7452ee1883c4d24b9374b03a6195ef3794b1b3a
Valentin Daguenet <valentin.daguenet@gmail.com>
2016-10-03 18:32:44 +0200

    init with vue and stylus

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const autoprefixer = require('autoprefixer-stylus');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, 'public/build'),
+    filename: 'build.js'
+  },
+  resolve: {
+    alias: { vue: 'vue/dist/vue.js' }
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.styl$/,
+        loader: ExtractTextPlugin.extract('css-loader!stylus-loader')
+      },
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }
+    ]
+  },
+  stylus: {
+    use: [autoprefixer()]
+  },
+  plugins: [
+    new ExtractTextPlugin('main.css')
+  ]
+}
