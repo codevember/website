194f3818aff7dac2d5586a077339298ea568e11b
tournevis <arthur.ro92@gmail.com>
2016-10-11 16:44:36 +0200

    Merge database inte 🔮

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import './style/index.styl';
 import Vue from 'vue';
 import router from './router';
+/*import Firebase from 'firebase';
+
+let firebase = new Firebase('https://codevember-af1d8.firebaseio.com/')*/
 
 new Vue({router}).$mount('#app');
--- src/views/home/Home.js
+++ src/views/home/Home.js
@@ -1,5 +1,15 @@
 import List from '../../components/list/List.js'
 import Proj from '../../components/project/Project.js'
+import firebase from 'firebase'
+var config = {
+  apiKey: "AIzaSyBsMjMcz4qhy8GtWeHQAq1S2oiO2HXYGb8",
+  authDomain: "codevembertest.firebaseapp.com",
+  databaseURL: "https://codevembertest.firebaseio.com",
+  storageBucket: "codevembertest.appspot.com",
+  messagingSenderId: "668336650459"
+};
+firebase.initializeApp(config)
+
 export default {
   name: 'Home',
   template: require('./home.html'),
@@ -14,6 +24,14 @@ export default {
   },
   mounted() {
     console.log('Hello from home');
+    let database = firebase.database().ref().child('projects');
+    database.on('value',function( snap ){
+      console.log(snap.val())
+    });
+
   },
+  ready(){
+
+ },
   methods: {}
 };
