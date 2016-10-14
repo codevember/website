4649e9d77f151af50f677462b06c3d0835ed3ee6
Valentin Daguenet <valentin.daguenet@gmail.com>
2016-10-14 20:07:35 +0200

    clean aliases

--- webpack.config.js
+++ webpack.config.js
@@ -14,8 +14,7 @@ module.exports = {
       components: path.join(__dirname, 'src/components'),
       layouts: path.join(__dirname, 'src/layouts'),
       views: path.join(__dirname, 'src/views'),
-      Api: path.join(__dirname, 'src/Api.js'),
-      DayListManager : path.join(__dirname, 'src/DayListManager.js')
+      Api: path.join(__dirname, 'src/Api.js')
     }
   },
   module: {
